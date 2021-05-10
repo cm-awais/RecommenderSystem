67c5dd3a35251f842739d4082082f666dd08ac83
Ch Muhammad Awais <37414048+Muhammad0Awais@users.noreply.github.com>
2021-05-10 11:49:25 +0300

    Update alternatingLeastSquareImplementation.py

--- src/alternatingLeastSquareImplementation.py
+++ src/alternatingLeastSquareImplementation.py
@@ -2,11 +2,6 @@ from scripts.dataLoader import loadData
 import numpy as np
 from sklearn.metrics import mean_squared_error
 
-trainPath = "../data/train.csv"
-
-data_df = loadData(trainPath)
-
-
 
 class ExplicitMF:
     """
@@ -28,10 +23,11 @@ class ExplicitMF:
         since lambda is a keyword in python we use reg instead
     """
 
-    def __init__(self, n_iters, n_factors, reg):
+    def __init__(self, n_iters, n_factors, reg, users_mean):
+        self.reg = reg
         self.n_iters = n_iters
         self.n_factors = n_factors  
-        self.reg = reg
+        self.user_means = users_mean
         
     def fit(self, train, test, lr):
         """
@@ -39,9 +35,11 @@ class ExplicitMF:
         model convergence, assuming both dataset is in the form
         of User x Item matrix with cells as ratings
         """
-        self.n_user, self.n_item = train.shape
-        self.user_factors = np.random.random((self.n_user, self.n_factors))
-        self.item_factors = np.random.random((self.n_item, self.n_factors))
+        self.n_user = train.shape[0]
+        self.n_item = train.shape[1]
+        
+        self.user_factors = np.random.rand(self.n_user, self.n_factors)
+        self.item_factors = np.random.rand(self.n_item, self.n_factors)
         
         # record the training and testing mse for every iteration
         # to show convergence later (usually, not worth it for production)
@@ -77,7 +75,7 @@ class ExplicitMF:
     
     def predict(self):
         """predict ratings for every user and item"""
-        pred = self.user_factors.dot(self.item_factors.T)
+        pred = self.user_factors.dot(self.item_factors.T) + self.user_means.reshape(-1,1)
         return pred
     
     @staticmethod
@@ -87,5 +85,30 @@ class ExplicitMF:
         mse = mean_squared_error(y_true[mask], y_pred[mask])
         return mse
 
-als = ExplicitMF(n_iters = 100, n_factors = 50, reg = 0.0001)
+
+trainPath = "../data/train.csv"
+
+data_df = loadData(trainPath)
+
+users_mean=np.array(data_df.mean(axis=1))
+R_demeaned=R_df.sub(data_df.mean(axis=1), axis=0)
+R_demeaned=R_demeaned.fillna(0).values
+
+def train_test_split(ratings):
+    test = np.zeros(ratings.shape)
+    train = ratings.copy()
+    for user in range(ratings.shape[0]):
+        test_ratings = np.random.choice(ratings[user, :].nonzero()[0], 
+                                        size=10, 
+                                        replace=False)
+        train[user, test_ratings] = 0.
+        test[user, test_ratings] = ratings[user, test_ratings]
+        
+    # Test and training are truly disjoint
+    assert(np.all((train * test) == 0)) 
+    return train, test
+
+test, train = train_test_split(R_demeaned)
+
+als = ExplicitMF(n_iters = 20, n_factors = 50, reg = 0.0001, users_mean= users_mean)
 als.fit(train, test)
