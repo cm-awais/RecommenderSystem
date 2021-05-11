69fc25a8bc2e71793074f9138fffa97cb86a6ba0
Ch Muhammad Awais <37414048+Muhammad0Awais@users.noreply.github.com>
2021-05-11 23:09:07 +0300

    Update alternatingLeastSquareImplementation.py

--- src/alternatingLeastSquareImplementation.py
+++ src/alternatingLeastSquareImplementation.py
@@ -167,3 +167,18 @@
 # already_rated, predictions = recommend_movies(preds_df, user, itemIds, traindf, 5)
 
 # print(predictions)
+
+
+
+from context import scripts
+
+from scripts.dataLoader import loadData
+from scripts.ExplicitModel import RunEALS
+from scripts.NN_Model import runNNModel
+
+trainPath = "../data/train.csv"
+
+R_df, traindf = loadData(trainPath)
+
+RunEALS(r_df=R_df, traindf=traindf)
+runNNModel(R_df, traindf)
