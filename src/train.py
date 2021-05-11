869c71637a789a4f6fab56d7e33242cb7e2319b8
Ch Muhammad Awais <37414048+Muhammad0Awais@users.noreply.github.com>
2021-05-11 23:09:13 +0300

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,12 +1,12 @@
-from context import scripts
+# from context import scripts
 
-from scripts.dataLoader import loadData
-from scripts.ExplicitModel import RunEALS
-from scripts.NN_Model import runNNModel
+# from scripts.dataLoader import loadData
+# from scripts.ExplicitModel import RunEALS
+# from scripts.NN_Model import runNNModel
 
-trainPath = "../data/train.csv"
+# trainPath = "../data/train.csv"
 
-R_df, traindf = loadData(trainPath)
+# R_df, traindf = loadData(trainPath)
 
-RunEALS(r_df=R_df, traindf=traindf)
-runNNModel(R_df, traindf)
+# RunEALS(r_df=R_df, traindf=traindf)
+# runNNModel(R_df, traindf)
